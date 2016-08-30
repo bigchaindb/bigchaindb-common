8905ac3c91b51fe65e1c3c76840eddbbd3fa4e8a
diminator <dimi.dejonghe@gmail.com>
2016-08-30 20:22:40 +0200

    versioning without last dot

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ docs_require = [
 
 setup(
     name='bigchaindb_common',
-    version='0.0.1.a4',
+    version='0.0.1a4',
     description="Set of reusable components for BigchainDB Python software.",
     long_description=readme + '\n\n' + history,
     author="BigchainDB",
